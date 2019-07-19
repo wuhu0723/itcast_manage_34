7591ec083cb3facf634d03d8d00ff6624584b2eb
wuhu <wuhu@itcast.cn>
2019-07-19 12:26:35 +0800

    06-实现登陆验证功能

--- src/views/login.vue
+++ src/views/login.vue
@@ -10,29 +10,67 @@
           <el-input v-model="loginForm.password" placeholder="密码"  prefix-icon="myicon myicon-key"></el-input>
         </el-form-item>
         <el-form-item>
-          <el-button type="primary" class="login-btn">登陆</el-button>
+          <el-button type="primary" class="login-btn" @click='login'>登陆</el-button>
         </el-form-item>
       </el-form>
     </div>
   </div>
 </template>
 <script>
+import { login } from '@/api/user_index.js'
 export default {
-  data() {
+  data () {
     return {
       loginForm: {
-        username: "",
-        password: ""
+        username: 'admin',
+        password: '123456'
       },
       rules: {
         username: [
-          { required: true, message: "请输入用户名", trigger: "blur" }
+          { required: true, message: '请输入用户名', trigger: 'blur' }
         ],
-        password: [{ required: true, message: "请输入密码", trigger: "blur" }]
+        password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
       }
-    };
+    }
+  },
+  methods: {
+    login () {
+      // 再次实现数据的验证
+      // 我们可以调用表单的validate方法实现数据的验证,在验证完成的时候,会调用传入的回调函数,这个回调函数有一个参数valid,如果这个参数为true,则说明验证通过(可以继续你的登陆请求),否则验证失败(阻止你的登陆请求并给出提示)
+      this.$refs.loginForm.validate((valid) => {
+        if (valid) {
+          login(this.loginForm)
+            .then(res => {
+              console.log(res)
+              if (res.data.meta.status === 200) {
+                // 进行跳转
+              } else {
+                this.$message({
+                  message: res.data.meta.msg,
+                  type: 'error'
+                })
+              }
+            })
+            .catch(err => {
+              console.log(err)
+              this.$message({
+                message: '登陆失败',
+                type: 'error'
+              })
+            })
+        } else {
+          this.$message({
+            message: '数据输入不合法',
+            type: 'error'
+          })
+          // 只有return false才能阻止请求
+          return false
+        }
+      })
+      // 发起请求
+    }
   }
-};
+}
 </script>
 <style lang="less" scoped>
 .login {
