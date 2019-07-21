3caef8ff216f6fd451391dc622e9b8f36889a381
wuhu <wuhu@itcast.cn>
2019-07-21 10:31:48 +0800

    09-左侧菜单项的添加及属性说明

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,11 +1,103 @@
 <template>
-    <div class="home">首页</div>
+  <div class="home">
+    <el-container>
+      <el-aside width="200px">
+        <div class="logo"></div>
+        <el-menu
+          :router='true'
+          :unique-opened='true'
+          :default-active="'1-3'"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item index="/users">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>用户列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+        <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-location"></i>
+              <span>权限管理</span>
+            </template>
+            <el-menu-item index="2-3">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>角色列表</span>
+              </template>
+            </el-menu-item>
+            <el-menu-item index="2-4">
+              <template slot="title">
+                <i class="el-icon-location"></i>
+                <span>权限列表</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+
+        </el-menu>
+      </el-aside>
+      <el-container>
+        <el-header>Header</el-header>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
 </template>
 <script>
-export default {
-
-}
+export default {}
 </script>
 <style lang="less" scoped>
-
+.home {
+  height: 100%;
+  .el-menu-admin:not(.el-menu--collapse) {
+    width: 200px;
+    min-height: 400px;
+  }
+  .el-container {
+    height: 100%;
+  }
+  .el-aside {
+    background-color: #545c64;
+  }
+  .el-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    background-color: #545c64;
+  }
+  .logo {
+    height: 60px;
+    background: url(../assets/logo.png);
+    background-size: cover;
+    background-color: #fff;
+  }
+  .toggle-btn {
+    padding: 0 15px;
+    margin-left: -20px;
+    font-size: 36px;
+    color: white;
+    cursor: pointer;
+    line-height: 60px;
+    &:hover {
+      background-color: #4292cf;
+    }
+  }
+  .system-title {
+    font-size: 28px;
+    color: white;
+  }
+  .welcome {
+    color: white;
+  }
+}
 </style>
