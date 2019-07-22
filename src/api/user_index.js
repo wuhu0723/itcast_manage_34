1788f91c25c5f9dbe9dea407d2671a11e9c49129
wuhu <wuhu@itcast.cn>
2019-07-22 11:18:48 +0800

    13-实现用户的添加和编辑功能

--- src/api/user_index.js
+++ src/api/user_index.js
@@ -23,3 +23,21 @@ export const getAllUserList = (params) => {
     params
   })
 }
+
+// 添加用户
+export const addUser = (data) => {
+  return axios({
+    url: 'users',
+    method: 'post',
+    data
+  })
+}
+
+// 编辑用户
+export const editUser = (data) => {
+  return axios({
+    url: `users/${data.id}`,
+    method: 'put',
+    data: { email: data.email, mobile: data.mobile }
+  })
+}
--- src/views/users/user.vue
+++ src/views/users/user.vue
@@ -13,11 +13,11 @@
         v-model="userobj.query"
         class="input-with-select"
         style="width:300px;margin-right:10px"
-        @keyup.enter.native='init'
+        @keyup.enter.native="init"
       >
-        <el-button slot="append" icon="el-icon-search" @click='init'></el-button>
+        <el-button slot="append" icon="el-icon-search" @click="init"></el-button>
       </el-input>
-      <el-button type="success" plain>添加用户</el-button>
+      <el-button type="success" plain @click='adddialogFormVisible = true'>添加用户</el-button>
     </div>
     <!-- 表格数据展示 -->
     <el-table :data="userList" style="width: 100%" border>
@@ -33,7 +33,7 @@
       <el-table-column label="操作" width="280">
         <template slot-scope="scope">
           <el-tooltip class="item" effect="dark" content="编辑" placement="top">
-            <el-button type="primary" icon="el-icon-edit"></el-button>
+            <el-button type="primary" icon="el-icon-edit" @click='showEditDialog(scope.row)'></el-button>
           </el-tooltip>
           <el-tooltip class="item" effect="dark" content="分配角色" placement="top">
             <el-button type="primary" icon="el-icon-d-caret"></el-button>
@@ -52,15 +52,70 @@
       :page-sizes="[1, 2, 3, 4]"
       :page-size="userobj.pagesize"
       layout="total, sizes, prev, pager, next, jumper"
-      :total="total">
-    </el-pagination>
+      :total="total"
+    ></el-pagination>
+
+    <!-- 添加用户对话框 -->
+    <el-dialog title="添加用户" :visible.sync="adddialogFormVisible">
+      <el-form :model="addform"  ref='addform'  :label-width="'80px'" :rules='rules'>
+        <el-form-item label="用户名" prop='username'>
+          <el-input v-model="addform.username" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="密码" prop='password'>
+          <el-input v-model="addform.password" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop='email'>
+          <el-input v-model="addform.email" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop='mobile'>
+          <el-input v-model="addform.mobile" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="adddialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click='addsubmit'>确 定</el-button>
+      </div>
+    </el-dialog>
+    <!-- 编辑用户对话框 -->
+    <el-dialog title="编辑用户" :visible.sync="editdialogFormVisible">
+      <el-form :model="editform"  ref='editform'  :label-width="'80px'" :rules='rules'>
+        <el-form-item label="用户名">
+          <el-input v-model="editform.username" auto-complete="off" disabled style='width:100px'></el-input>
+        </el-form-item>
+        <el-form-item label="邮箱" prop='email'>
+          <el-input v-model="editform.email" auto-complete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="手机号" prop='mobile'>
+          <el-input v-model="editform.mobile" auto-complete="off"></el-input>
+        </el-form-item>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button @click="editdialogFormVisible = false">取 消</el-button>
+        <el-button type="primary" @click='editsubmit'>确 定</el-button>
+      </div>
+    </el-dialog>
   </div>
 </template>
 <script>
-import { getAllUserList } from '@/api/user_index.js'
+import { getAllUserList, addUser, editUser } from '@/api/user_index.js'
 export default {
   data () {
     return {
+      editdialogFormVisible: false,
+      editform: {
+        id: '',
+        username: '',
+        email: '',
+        mobile: ''
+      },
+      adddialogFormVisible: false,
+      // 添加用户对应的数据对象
+      addform: {
+        username: '',
+        password: '',
+        email: '',
+        mobile: ''
+      },
       // 总记录数
       total: 0,
       value2: true,
@@ -68,13 +123,96 @@ export default {
       userobj: {
         query: '',
         pagenum: 1,
-        pagesize: 1
+        pagesize: 4
       },
       //   表格数据源,它是一个数组,里面的每个元素都是一个对象
-      userList: []
+      userList: [],
+      // 表单验证规则
+      rules: {
+        username: [
+          { required: true, message: '请输入用户名', trigger: 'blur' }
+        ],
+        password: [
+          { required: true, message: '请输入密码', trigger: 'blur' }
+        ],
+        email: [
+          { required: true, message: '请输入邮箱', trigger: 'blur' },
+          // 添加正则验证规则
+          { pattern: /\w+[@]\w+[.]\w+/, message: '请输入合法的电子邮箱', trigger: 'blur' }
+        ],
+        mobile: [
+          { required: true, message: '请输入手机号', trigger: 'blur' },
+          { pattern: /^1\d{10}$/, message: '请输入合法的手机号', trigger: 'blur' }
+        ]
+      }
     }
   },
   methods: {
+    // 编辑提交
+    async editsubmit () {
+      // await：必须写在async函数内
+      let res = await editUser(this.editform)
+      console.log(res)
+      if (res.data.meta.status === 200) {
+        this.$message({
+          type: 'success',
+          message: '数据编辑成功'
+        })
+        // 隐藏对话框
+        this.editdialogFormVisible = false
+        // 重置表单元素的数据
+        this.$refs.editform.resetFields()
+        // 刷新
+        this.init()
+      } else {
+        this.$message({
+          type: 'error',
+          message: '数据编辑失败'
+        })
+      }
+    },
+    // 显示编辑对话框
+    showEditDialog (row) {
+      this.editdialogFormVisible = true
+      this.editform.id = row.id
+      this.editform.username = row.username
+      this.editform.email = row.email
+      this.editform.mobile = row.mobile
+    },
+    // 添加用户
+    addsubmit () {
+      // 对表单元素的数据进行验证
+      this.$refs.addform.validate(valid => {
+        if (valid) {
+          // 调用接口方法
+          addUser(this.addform)
+            .then(res => {
+              console.log(res)
+              if (res.data.meta.status === 201) {
+                this.$message({
+                  type: 'success',
+                  message: '数据添加成功'
+                })
+                // 隐藏对话框
+                this.adddialogFormVisible = false
+                // 重置表单元素的数据
+                this.$refs.addform.resetFields()
+                // 刷新
+                this.init()
+              }
+            })
+            .catch(err => {
+              console.log(err)
+            })
+        } else {
+          this.$message({
+            type: 'error',
+            message: '数据输入不合法'
+          })
+          return false
+        }
+      })
+    },
     // 切换每页显示数量时触发
     handleSizeChange (val) {
       console.log(`每页 ${val} 条`)
