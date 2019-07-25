9602973519d0e14fc26298a1d6022593334392fb
wuhu <wuhu@itcast.cn>
2019-07-25 12:57:44 +0800

    23-实现左侧导航菜单的动态生成，完成完整的权限管理体验

--- src/views/home.vue
+++ src/views/home.vue
@@ -4,68 +4,60 @@
       <el-aside width="auto">
         <div class="logo"></div>
         <el-menu
-          :collapse='iscollapse'
-          :router='true'
-          :unique-opened='true'
+          :collapse="iscollapse"
+          :router="true"
+          :unique-opened="true"
           :default-active="'1-3'"
           class="el-menu-vertical-demo"
           background-color="#545c64"
           text-color="#fff"
           active-text-color="#ffd04b"
         >
-          <el-submenu index="1">
+          <el-submenu :index="first.id+''" v-for='first in menuList' :key='first.id'>
             <template slot="title">
               <i class="el-icon-location"></i>
-              <span>用户管理</span>
+              <span>{{first.authName}}</span>
             </template>
-            <el-menu-item index="/home/users">
+            <el-menu-item :index="'/home/'+second.path" v-for='second in first.children' :key='second.id'>
               <template slot="title">
                 <i class="el-icon-location"></i>
-                <span>用户列表</span>
+                <span>{{second.authName}}</span>
               </template>
             </el-menu-item>
           </el-submenu>
-
-        <el-submenu index="2">
-            <template slot="title">
-              <i class="el-icon-location"></i>
-              <span>权限管理</span>
-            </template>
-            <el-menu-item index="/home/roles">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>角色列表</span>
-              </template>
-            </el-menu-item>
-            <el-menu-item index="/home/rights">
-              <template slot="title">
-                <i class="el-icon-location"></i>
-                <span>权限列表</span>
-              </template>
-            </el-menu-item>
-          </el-submenu>
-
         </el-menu>
       </el-aside>
       <el-container>
         <el-header>
-            <span class="myicon myicon-menu toggle-btn" @click="iscollapse = !iscollapse"></span>
-            <span class="system-title">电商后台管理系统</span>
-            <a href="javascrpt:;" class="welcome">退出</a>
+          <span class="myicon myicon-menu toggle-btn" @click="iscollapse = !iscollapse"></span>
+          <span class="system-title">电商后台管理系统</span>
+          <a href="javascrpt:;" class="welcome">退出</a>
         </el-header>
         <el-main>
-            <router-view></router-view>
+          <router-view></router-view>
         </el-main>
       </el-container>
     </el-container>
   </div>
 </template>
 <script>
+import { getLeftMenu } from '@/api/right_index.js'
 export default {
   data () {
     return {
-      iscollapse: false
+      iscollapse: false,
+      menuList: []
     }
+  },
+  mounted () {
+    getLeftMenu()
+      .then(res => {
+        console.log(res)
+        this.menuList = res.data.data
+      })
+      .catch(err => {
+        console.log(err)
+      })
   }
 }
 </script>
@@ -73,7 +65,7 @@ export default {
 .home {
   height: 100%;
   .el-menu {
-      width: auto;
+    width: auto;
   }
   // 如果是展开状态,那么宽度就是200px,如果是合并状态,宽度:auto
   .el-menu:not(.el-menu--collapse) {
